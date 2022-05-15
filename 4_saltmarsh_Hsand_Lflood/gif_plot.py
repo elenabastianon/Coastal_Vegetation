8c3b07694ff0f688ad05e67d16e149e0d8abf236
elenabastianon <47004293+elenabastianon@users.noreply.github.com>
2022-05-15 21:28:26 +0100

    Add files via upload

---
+++ 4_saltmarsh_Hsand_Lflood/gif_plot.py
@@ -0,0 +1,10 @@
+# Gif maker code
+
+import imageio
+#load images
+frames =[]
+for i in range (833,5000,833):
+    filename = "1delta" + str(i) + ".png"
+    
+frames.append(imageio.imread(filename))
+imageio.mimwrite("saltmarshes_75flood_075bedload.gif", frames, format = ".gif", fps = 1)
